2533be162d1b79ee367c5ac1263c3facd564d2eb
siniseong <shinhuiseong07@gmail.com>
2025-03-10 12:24:25 +0900

    chore :: 아이콘 추가

--- app/Stock/Stock.tsx
+++ app/Stock/Stock.tsx
@@ -27,10 +27,22 @@ export default function Stock() {
     { name: '미미미누', count: 190, price: 32000, category: '학습/공부', volume: '500,000주', rate: '+0.5%', liked: false },
   ]);
 
-  const periods = ['실시간', '1시간', '6시간', '1일', '1주일', '1개월'];
+  const periods = [
+    '실시간', 
+    '1시간', 
+    '6시간', 
+    '1일', 
+    '1주일', 
+    '1개월'
+  ];
 
-  const weekTitle = <h1 className="text-black mt-[-6px] text-[22px] font-semibold">모든 주식 목록</h1>;
-  const subTitle = <h2 className="text-gray-700 text-[15px] mb-4">대한민국의 150만 이상 유튜브 채널에 대한 모든 주식 목록이에요.</h2>;
+  const weekTitle = (
+    <div className="flex items-center">
+      <h1 className="text-black mt-[-6px] text-[22px] font-semibold">모든 주식 목록</h1>
+      {/* <span className="text-blue-900 text-sm ml-4">오늘 12시 20분 기준</span> */}
+    </div>
+  );
+  const subTitle = <h2 className="text-gray-700 text-[15px] mb-4">대한민국 150만 이상 유튜브 채널에 대한 모든 주식 목록이에요.</h2>;
 
   function toggleFavorite(index: number) {
     const newSubscribers = [...subscribers];
@@ -83,7 +95,15 @@ export default function Stock() {
             }`}
             onClick={() => setSelectedPeriod(period)}
           >
-            {period}
+            {period === '실시간' && (
+              <span className="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4 mr-1">
+                  <path fillRule="evenodd" d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z" clipRule="evenodd" />
+                </svg>
+                <span className={selectedPeriod === '실시간' ? 'text-blue-700' : 'text-gray-700'}>{period}</span>
+              </span>
+            )}
+            {period !== '실시간' && period}
           </button>
         ))}
       </div>
