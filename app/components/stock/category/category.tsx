caeb684dac3bd7e351f3d0decc2d04b6c14bc4cb
siniseong <shinhuiseong07@gmail.com>
2025-03-11 09:42:58 +0900

    chore :: /로 텍스트 입력 가능을 알려주는 문구 추가

--- app/components/stock/category/category.tsx
+++ app/components/stock/category/category.tsx
@@ -159,7 +159,7 @@ export default function Category({
         appliedMax={appliedMax}
         setAppliedMax={setAppliedMax}
       />
-      <div className="relative inline-block">
+      <div className="relative inline-block ml-220 mb-4">
         <button
           type="button"
           onClick={() => {
@@ -177,15 +177,20 @@ export default function Category({
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              className="size-4 text-gray-900"
+              className="size-4 text-gray-700"
             >
               <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
             </svg>
             <input
               type="text"
               placeholder="채널명을 검색하세요."
-              className="ml-2 border-none focus:outline-none text-gray-700"
+              className="ml-2 border-none focus:outline-none"
             />
+            <div className="absolute right-3">
+              <span className="text-gray-600 text-xs px-1.5 py-0.5 rounded border border-gray-300 bg-gray-50">
+                / 
+              </span>
+            </div>
           </span>
         </button>
       </div>
